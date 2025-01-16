cd44c2f4f9a1a3ed24680161419bdc23c852fc3b
ZegarekPL <zegarekpl.gaming@gmail.com>
2025-01-16 02:50:11 +0100

    m

---
+++ Migrations/20250116013322_Grade4.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project_court_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Grade4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "userGrade",
+                table: "Court");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "userGrade",
+                table: "Court",
+                type: "integer",
+                nullable: true);
+        }
+    }
+}
